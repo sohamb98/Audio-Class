678855ebeb7938663a1aae52091caa3a65eab98f
sohamb98 <bhattacharya.soham@outlook.com>
2022-07-01 21:07:21 +0100

    Created a feature extractor

--- spectogram.py
+++ spectogram.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
+from tqdm import tqdm
 
 from glob import glob
 
@@ -14,10 +16,13 @@ from playsound import playsound
 #Reading metadata
 metadata = pd.read_csv(r"D:\Audio Dataset\TAU-urban-acoustic-scenes-2020-mobile-development\meta.csv", delim_whitespace=True)
 metadata.head()
-print(metadata['filename'][0])
+#print(metadata['filename'][0])
 
 #Redeading metadata from the folder
 audio_files = glob("D:\\Audio Dataset\\TAU-urban-acoustic-scenes-2020-mobile-development\\audio\\*.wav")
+
+
+'''''
 #print(audio_files[1])
 playsound(audio_files[0])
 
@@ -57,3 +62,36 @@ fig, ax = plt.subplots(figsize=(15,5))
 img = librosa.display.specshow(s1_db_mel, x_axis="time", y_axis="log")
 fig.colorbar(img, ax=ax, format=f'%0.2f')
 plt.show()
+'''''
+
+def features_extracter(file):
+    y, sr = librosa.load(file)
+    s1 = librosa.feature.melspectrogram(y=y,sr = sr, n_mels=128*4)
+    s1_db_mel = librosa.amplitude_to_db(np.abs(s1), ref=np.max)
+
+    return s1_db_mel
+
+def main():
+    features = []
+    records = len(metadata.index)
+    for index_num,row in metadata.iterrows():
+        filename = os.path.abspath( row['filename'] )
+        final_class_labels = row['scene_label']
+        feature = features_extracter(filename)
+        features.append([feature, final_class_labels])
+        os.system('cls')
+        print(f'Extracted file{index_num}/{records}')
+
+
+    
+    #print(os.path.abspath( metadata['filename'][0] ))
+    #print(os.path.dirname(os.path.abspath(__file__)))
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
